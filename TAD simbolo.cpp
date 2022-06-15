2a18232d03da43c658dd1e4ca82fd1cffbe9f578
Luis Robles <luis.roblessorroche@alum.uca.es>
2022-06-15 17:56:17 +0200

    añadido tad simbolo y tad tictactoe

---
+++ TAD simbolo.cpp
@@ -0,0 +1,201 @@
+//TAD SIMBOLO
+
+/*
+El TAD simbolo se usa para representar simbolos trazados con lineas rectas. Un simbolo es
+una sucesion de trazos, un trazo es una linea recta que puede tener un sentido a izquierda,
+derecham arriba o abajo. El TAD simbolo tiene que permitir realizar las siguientes operaciones con un simbolo:
+	- Crear un simbolo vacio
+	- Añadir un trazo al final de un simbolo
+	- Deshacer los ultimos n trazos
+	- Simetrico con respecto a eje x
+	- Simetrico con respecto a eje y
+	- Simetrico con respecto a eje xy
+*/
+
+
+/*
+Para realizar este ejercicio haremos uso de pilas dinamicas(enlazadas).
+La estructura de una pila enlazada es la siguiente:
+*/
+
+struc nodo
+{
+	T elto;
+	nodo* sig;
+	nodo(const T& e, nodo* p = nullptr): elto(e), sig(p){}
+}
+nodo* tope_;
+
+//dicha pila estara formada por trazos, que en nuestro caso van a ser caracteres.
+// la equivalencia será: w(arriba), a(izquierda), s(abajo), d(derecha)
+
+//Las operaciones del TAD pila que vamos a usar serán las siguientes:
+
+void push(const T& x);
+void pop();
+const T& tope() const;
+bool vacia() const;
+
+//La clase que vamos a crear será la siguiente:
+
+class Simbolo
+{
+	public:
+		Simbolo();
+		void anadir_trazo(char trazo);
+		void eliminar_n_trazos(int n);
+		char simetrico_x() const;
+		char simetrico_y() const;
+		char simetrico_xy() const;
+	private:
+		Pila<char>P;
+}
+
+//Las operciones serán las siguientes:
+
+//precondicion:
+//postcondicion:crea un simbolo vacio
+Simbolo::Simbolo()
+{
+
+}
+
+
+//precondicion:
+//postcondicion: inserta un nuevo trazo al final del simbolo
+void Simbolo::anadir_trazo(char trazo)
+{
+	P.push(trazo);
+}
+
+//precondicion: la pila tiene mas o los mismos trazos que se desean eliminar
+//postcondicion: elimina los n ultimos trazos
+void Simbolo::eliminar_n_trazos(int n)
+{
+	Pila<char>aux(P);
+	int cont = 0;
+	while(!aux.vacia())
+	{
+		aux.pop();
+		cont++;
+	}
+
+	assert(cont > n);
+
+	while(n>0)
+	{
+		P.pop();
+		n--;
+	}
+}
+
+//precondicion:
+//postcondicion: devuelve el simetrico del simbolo con respecto al eje x
+char Simbolo::simetrico_x() const
+{
+	Pila<char>aux(P);//para no modificar la pila original
+	Pila<char>aux2;
+
+	while(!aux.vacia())
+	{
+		if(aux.tope() == 'w')
+		{
+			aux2.push('s');
+		}
+		else
+		{
+			if(aux.tope() == 's')
+			{
+				aux2.push('w');
+			}
+			else
+			{
+				aux2.push(aux.tope());
+			}
+		}
+		aux.pop();
+	}
+
+	while(!aux2.vacia())
+	{
+		aux.push(aux2.tope());
+		aux2.pop();
+	}
+	return aux;
+}
+
+//precondicicion:
+//postcondicion: devuelve el simetrico del simbolo con respecto al eje y
+char Simbolo::simetrico_y() const
+{
+	Pila<char>aux(P);//para no modificar la pila original
+	Pila<char>aux2;
+
+	while(!aux.vacia())
+	{
+		if(aux.tope() == 'a')
+		{
+			aux2.push('d');
+		}
+		else
+		{
+			if(aux.tope() == 'd')
+			{
+				aux2.push('a');
+			}
+			else
+			{
+				aux2.push(aux.tope());
+			}
+		}
+		aux.pop();
+	}
+
+	while(!aux2.vacia())
+	{
+		aux.push(aux2.tope());
+		aux2.pop();
+	}
+	return aux;
+}
+
+//precondicion:
+//postcondicion: devuelve el simetrico del simbolo con respecto al eje x e y
+char Simbolo::simetrico_xy() const
+{
+	Pila<char>aux(P);//copia para no modificar la pila original
+	PIla<char>aux2;
+
+	while(!aux.vacia())
+	{
+		if(aux.tope() == 'a')
+		{
+			aux2.push('d');
+		}
+		else
+		{
+			if(aux.tope() == 'd')
+			{
+				aux2.push('a');
+			}
+			else
+			{
+				if(aux.tope() == 'w')
+				{
+					aux2.push('s');
+				}
+				else
+				{
+					aux2.push('w');
+				}
+			}
+		}
+		aux.pop();
+	}
+	while(aux2.vacia())
+	{
+		aux.push(aux2.tope());
+		aux2.pop();
+	}
+	return aux;
+}
---
+++ TAD tictactoe.cpp
@@ -0,0 +1,126 @@
+//TAD tic-tac-toe
+
+/*
+Representar el TAD tablero tic tac toe con las siguientes funciones:
+	- Crear tablero vacio
+	- Colocar ficha de un jugador en la casilla determinada
+	- Devolver un conjunto de las casillas vacias
+	- devolver la ficha de una casilla
+	- comprobar si un jugador determinado tiene 3 fichas en raya
+	- comprobar si la partida ha acabdo en tablas(empate)
+	- destruir el tablero
+*/
+
+
+/*
+Para este ejercicio no haremos uso de ningun tad conocido ya que es mas facil hacerlo mediante un vector.
+Para ello usare un vector de la stl.
+Al tratarse de un 3 en raya, solo necesitaremos un vector de 9 posiciones (de 0 a 8)
+El tablero tendra las casillas con - si está vacio, y si no lo está, las posibles fichas serán x e o
+*/
+
+//las operaciones que empleare del la clase vector sera:
+const T& at(int pos); //para acceder al elemento de esa posicion del vector
+void push_back(t& x); //insertar elemento al final del vector
+
+
+//la clase será la siguiente:
+class TicTacToe()
+{
+	public:
+		TicTacToe();
+		void colocar_ficha(char ficha, int casilla);
+		int casillas_vacias() const;
+		char ficha_casilla(int casilla);
+		bool victoria(char ficha) const;
+		bool empate() const;
+		~TicTacToe();
+	private:
+		vec<char>v;
+}
+
+//precondicion:
+//postcondicion: crea un tablero vacio(de 9 casillas)
+TicTacToe::TicTacToe()
+{
+	v = new vec<char>v(9);
+
+	for(int i = 0; i<9; i++)
+	{
+		v.at(i) = -; //inicializar tablero vacio(todo a -)
+	}
+}
+
+//precondicion: la ficha es x o o y la casilla esta entre las disponibles y no esta ocupada
+//postcondicion: inserta la ficha en esa casilla
+void TicTacToe::colocar_ficha(char ficha, int casilla)
+{
+	assert(ficha == 'x' || ficha == 'o');
+	assert(casilla >= 0 && casilla <= 8);
+	assert(v.at(casilla) == '-');
+
+	v.at(casilla) = ficha;
+}
+
+//precondicion:
+//postcondicion:devuelve una lista con las casillas vacias
+int TicTacToe::casillas_vacias() const
+{
+	vec<int> v2;
+	for(int i = 0; i<9;i++)
+	{
+		if(v.at(i) == '-')
+		{
+			v2.push_back(i);
+		}
+	}
+	return v2;
+}
+
+
+//precondicion: la casilla esta entre el rango permitido (mayor o igual a 0 y menor que 9)
+//postcondicion: devuelve la ficha de esa posicion
+char TicTacToe::ficha_casilla(int casilla)
+{
+	assert(casilla >= 0 && casilla < 9);
+	return v.at(casilla);
+}
+
+//precondicion: la ficha es una de las disponibles
+//postcondicion: devuelve true si el jugador especifico ha ganado y false si no
+bool TicTacToe::victoria(char ficha) const
+{
+	assert(ficha == 'x' || ficha == 'o');
+
+	return ((v.at(0) == ficha && v.at(1) == ficha && v.at(2) == ficha) ||
+			(v.at(3) == ficha && v.at(4) == ficha && v.at(5) == ficha) ||
+			(v.at(6) == ficha && v.at(7) == ficha && v.at(8) == ficha) ||
+			(v.at(0) == ficha && v.at(3) == ficha && v.at(6) == ficha) ||
+			(v.at(1) == ficha && v.at(4) == ficha && v.at(7) == ficha) ||
+			(v.at(2) == ficha && v.at(5) == ficha && v.at(8) == ficha) ||
+			(v.at(0) == ficha && v.at(4) == ficha && v.at(8) == ficha) ||
+			(v.at(2) == ficha && v.at(4) == ficha && v.at(6) == ficha));
+}
+
+//precondicion: el tablero esta entero ocupado
+//postcondicion: devuelve true si hay empate y false si no
+bool TicTacToe::empate()
+{
+	for (int i = 0; i<9;i++)
+	{
+		if(v.at(i) == '-')
+		{
+			assert(false);
+		}
+	}
+	char x = 'x';
+	char o = 'o';
+	return(!victoria(x) && !victoria(o));
+}
+
+//precondicion:
+//postcondicion:elimina el tablero
+TicTacToe::~TicTacToe()
+{
+	
+}
